24cd47428eb398fb558aab4069ac2054ea87d553
Tim Havlicek <tim.h4vlicek@gmail.com>
2021-04-01 14:22:53 +0200

    server

--- app/net/udp-server.js
+++ app/net/udp-server.js
@@ -2,9 +2,13 @@ const dgram = require("dgram");
  
 let server = dgram.createSocket("udp4");
 
-server.bind(() => { server.setBroadcast(true); setInterval(broadcastGame, 3000); });
+server.bind(() => { server.setBroadcast(true); setInterval(broadcast, 3000); });
 
-const broadcastGame = () => { 
-    let message = new Buffer("displayGame"); 
+const broadcast = () => { 
+    let message = new Buffer(JSON.stringify({
+        timestamp: Date.now(),
+        message: "Testing UDP braodcast."
+    })); 
     server.send(message, 0, message.length, 3001, "255.255.255.255");
+    console.log('Sent broadcast message.');
 };
