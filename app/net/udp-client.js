9156e7f0b20adc31d3e7cde599262e06c979ee74
Tim Havlicek <tim.h4vlicek@gmail.com>
2021-04-01 14:20:08 +0200

    dgfgdf

--- app/net/udp-client.js
+++ app/net/udp-client.js
@@ -1,28 +1,9 @@
-// Require node js dgram module.
-var dgram = require('dgram');
+let client = dgram.createSocket("udp4");
 
-// Create a udp socket client object.
-var client = dgram.createSocket("udp4");
+client.on("listening", () => { client.setBroadcast(true); });
 
-// message variable is used to save user input text.
-var message = "";
+client.bind(3001);
 
-// Set command line input character encoding to utf-8.
-process.stdin.setEncoding('utf-8');
-
-// When receive user input data in console.
-process.stdin.on('data', function (text) {
-    // If user do not input data in command line then send default message.
-    if (message == null || message.length == 0) {
-        message = "Hello udp server.";
-
-    }
-
-    console.log("User input : " + message);
-
-    // Create a node Buffer object to wrap message object.
-    message = new Buffer(message);
-
-    // Send message to udp server through client socket.
-    client.send(message, 0, message.length, 48260, "localhost");
+client.on("message", (message, rinfo) => {
+    console.log(`Message from: ${rinfo.address}:${rinfo.port} - ${message}`); 
 });
--- app/net/udp-server.js
+++ app/net/udp-server.js
@@ -1,23 +1,10 @@
-// Require dgram module.
-var dgram = require('dgram');
+const dgram = require("dgram"); 
+ 
+let server = dgram.createSocket("udp4");
 
-// Create udp server socket object.
-var server = dgram.createSocket("udp4");
+server.bind(() => { server.setBroadcast(true); setInterval(broadcastGame, 3000); });
 
-// Make udp server listen on port 8089.
-server.bind(48260);
-
-// When udp server receive message.
-server.on("message", function (message) {
-    // Create output message.
-    var output = "Udp server receive message : " + message + "\n";
-    // Print received message in stdout, here is log console.
-    process.stdout.write(output);
-});
-
-// When udp server started and listening.
-server.on('listening', function () {
-    // Get and print udp server listening ip address and port number in log console. 
-    var address = server.address(); 
-    console.log('UDP Server started and listening on ' + address.address + ":" + address.port);
-});
+const broadcastGame = () => { 
+    let message = new Buffer("displayGame"); 
+    server.send(message, 0, message.length, 3001, "255.255.255.255");
+};
