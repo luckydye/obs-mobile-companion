dd2e29185642166b5ad0656ba80676748a46889e
luckydye <tim.h4vlicek@gmail.com>
2021-04-01 15:16:09 +0200

    udp client

--- app/net/udp-client.js
+++ app/net/udp-client.js
@@ -1,17 +1,37 @@
 const dgram = require('dgram');
 let client = dgram.createSocket("udp4");
 
+const PORT = 3001;
+
 client.on("listening", () => {
     client.setBroadcast(true); 
 });
 
-client.bind(3001);
+client.bind(PORT);
+
+let foundServer = false;
 
 client.on("message", (message, rinfo) => {
     console.log(`Message from: ${rinfo.address}:${rinfo.port}`);
     try {
         const json = JSON.parse(message);
+        console.log(`Message: ${JSON.stringify(json)}`);
 
-        console.log(`Message: ${json.message}`);
+        if(json.type == "pong") {
+            const roundtrap = Date.now() - (+json.timestamp);
+            console.log(`Latency: ${roundtrap / 2}ms`);
+        }
+
+        if(!foundServer) {
+            let message = new Buffer(JSON.stringify({
+                type: "ping",
+                id: Math.floor(Math.random() * 100000000).toString(),
+                timestamp: Date.now(),
+            }));
+            client.send(message, 0, message.length, PORT, rinfo.address);
+            console.log('Sent reply');
+
+            foundServer = true;
+        }
     } catch(err) {}
 });
