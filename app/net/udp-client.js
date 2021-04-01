0feaf41ce9e6661fb7021690d51b0f2734f17c2e
luckydye <tim.h4vlicek@gmail.com>
2021-04-01 14:31:57 +0200

    client

--- app/net/udp-client.js
+++ app/net/udp-client.js
@@ -1,9 +1,17 @@
+const dgram = require('dgram');
 let client = dgram.createSocket("udp4");
 
-client.on("listening", () => { client.setBroadcast(true); });
+client.on("listening", () => {
+    client.setBroadcast(true); 
+});
 
 client.bind(3001);
 
 client.on("message", (message, rinfo) => {
-    console.log(`Message from: ${rinfo.address}:${rinfo.port} - ${message}`); 
+    console.log(`Message from: ${rinfo.address}:${rinfo.port}`);
+    try {
+        const json = JSON.parse(message);
+
+        console.log(`Message: ${json.message}`);
+    } catch(err) {}
 });
