76029143a85691a847b14bb599607085f6c79809
Tim Havlicek <tim.h4vlicek@gmail.com>
2021-05-30 14:07:35 +0200

    no canvas preview

--- public/components/OBSPreview.js
+++ public/components/OBSPreview.js
@@ -35,14 +35,7 @@ async function createRTCAnswer(remoteOffer, callback) {
 }
 
 function getMedia(callback) {
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext("2d");
-
-    canvas.width = 1920;
-    canvas.height = 1080;
-
-    let fps = 0;
-    let lastFrame = 0;
+    const video = document.createElement('video');
 
     const socket = io(location.host);
 
@@ -65,7 +58,6 @@ function getMedia(callback) {
     })
 
     function handleRemoteStream(stream) {
-        const video = document.createElement('video');
         video.srcObject = stream;
         video.muted = true;
 
@@ -73,19 +65,11 @@ function getMedia(callback) {
             video.play();
         }
 
-        callback(stream, video);
-
-        const draw = () => {
-            context.drawImage(video, 0, 0, canvas.width, canvas.height);
-            requestAnimationFrame(draw);
-        }
-        draw();
+        callback(stream);
     }
 
-    return canvas;
+    return video;
 }
-
-let ticksPerSeconds;
 
 class OBSPreview extends LitElement {
 
@@ -94,34 +78,17 @@ class OBSPreview extends LitElement {
             :host {
                 display: block;
             }
+            video {
+                width: 100%;
+                height: 100%;
+                object-fit: contain;
+                pointer-events: none;
+            }
         `;
     }
 
     constructor() {
         super();
-
-        this.canvas = document.createElement('canvas');
-        this.context = this.canvas.getContext("2d");
-
-        this.sourceWidth = 1920;
-        this.sourceHeight = 1080;
-
-        window.addEventListener('resize', e => {
-            this.reformatCanvas();
-        });
-
-        this.lastTick = 0;
-
-        const loop = (currtick) => {
-            const delta = currtick - this.lastTick;
-            ticksPerSeconds = 1000 / delta;
-
-            this.renderCanvas();
-            requestAnimationFrame(loop);
-
-            this.lastTick = currtick;
-        }
-        loop();
     }
 
     getStream() {
@@ -132,93 +99,20 @@ class OBSPreview extends LitElement {
         return this.video;
     }
 
-    reformatCanvas() {
-        this.canvas.width = this.clientWidth;
-        this.canvas.height = this.clientHeight;
-    }
-
     connectedCallback() {
         super.connectedCallback();
-        this.reformatCanvas();
 
-        this.video = getMedia((stream, video) => {
+        this.video = getMedia((stream) => {
             this.stream = stream;
-            this.videoSource = video;
             this.dispatchEvent(new Event('stream'));
         });
         
         this.ready = true;
     }
 
-    get preivewFps() {
-        return Math.floor(ticksPerSeconds);
-    }
-
-    renderCanvas() {
-        if (!this.canvas)
-            return;
-
-        const sw = this.sourceWidth;
-        const sh = this.sourceHeight;
-
-        const dw = this.canvas.width;
-        const dh = this.canvas.height;
-
-        const minW = Math.min(dw, sw);
-        const minH = Math.min(dh, sh);
-
-        this.context.clearRect(0, 0, dw, dh);
-
-        const borderPadding = 80;
-
-        const sar = sw / sh;
-
-        let w = minW;
-        let h = minW / sar;
-
-        if (dh < h) {
-            w = minH * sar;
-            h = minH;
-        }
-
-        const borderPaddingX = 0;
-        const borderPaddingY = 0;
-
-        const cx = (dw / 2) - (w / 2);
-        const cy = (dh / 2) - (h / 2);
-        // this.context.fillStyle = "#00ff00";
-        // this.context.strokeStyle = "#eee";
-        // this.context.strokeRect(
-        //     borderPaddingX + cx,
-        //     borderPaddingY + cy,
-        //     w - (borderPaddingX * 2),
-        //     h - (borderPaddingY * 2)
-        // );
-
-        if(this.video) {
-            this.context.drawImage(
-                this.video, 
-                0, 0,
-                this.video.width, 
-                this.video.height,
-                borderPaddingX + cx,
-                borderPaddingY + cy,
-                w - (borderPaddingX * 2),
-                h - (borderPaddingY * 2),
-            )
-        }
-
-        // // overlay text
-        // this.context.fillStyle = "#eee";
-        // this.context.fillText(`${sw}x${sh}`, 20, dh - 20);
-
-        // this.context.fillStyle = "#eee";
-        // this.context.fillText(`${this.preivewFps}fps`, 120, dh - 20);
-    }
-
     render() {
         return html`
-            ${this.canvas}
+            ${this.video}
         `;
     }
 
